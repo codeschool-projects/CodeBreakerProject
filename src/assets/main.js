59bfd13ee6f89d92b04e4726020b81026650efe2
Eric J Fisher <EricJFisher@Live.com>
2017-01-06 16:18:08 -0500

    Revamped tasks to fit refactoring, added comments

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,19 +1,24 @@
+//Get answer and attempt inputs for use throughout page
 let answer = document.getElementById('answer').value;
 let attempt = document.getElementById('attempt').value;
 
 function guess(){
+    //Get user input
     let input = document.getElementById('user-guess').value;
 
-    setMessage('');
-
+    //Set Answer and Attempt if either is empty (new game)
     if(answer == "" || attempt == "") {
         setHiddenFields();
     }
 
+    //Ensure user input is valid, iterate attempt if valid
     if(!validateInput(input)){
         return;
+    } else {
+        attempt++;
     }
 
+    //Check user input against answer and display results
     if(getResults(input)) {
         setMessage('You Win! :)');
         showAnswer(true);
@@ -27,6 +32,7 @@ function guess(){
     }
 }
 
+//function takes user input, check against the answer, and displays results
 function getResults(input){
     let correct = 0;
     let html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
@@ -53,6 +59,7 @@ function getResults(input){
     }
 }
 
+//function sets hidden fields
 function setHiddenFields() {
     answer = Math.floor(Math.random() * 10000).toString();
     while(answer.length < 4) {
@@ -61,10 +68,12 @@ function setHiddenFields() {
     attempt = 0;
 }
 
+//function sets the message to supplied string
 function setMessage(message){
     document.getElementById('message').innerHTML = message;
 }
 
+//function shows the answer and colors based on if the player won or lost
 function showAnswer(success){
     let code = document.getElementById('code');
     if(success) {
@@ -75,17 +84,17 @@ function showAnswer(success){
     code.innerHTML = answer;
 }
 
+//function hides the guessing-div and shows the replay-div to allow replay
 function showReplay(){
     document.getElementById('guessing-div').style = "display:none";
     document.getElementById('replay-div').style = "display:block";
 }
 
+//function ensure input was 4 characters long
 function validateInput(input) {
     if(input.length != 4) {
         setMessage('Guesses must be exactly 4 characters long.');
         return false;
-    } else {
-        attempt++;
-        return true;
-    }
+    } 
+    return true;
 }
