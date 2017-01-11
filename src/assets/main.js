727eb7402759c053ad49689ee92c2eb63f30ea31
Eric J Fisher <EricJFisher@Live.com>
2017-01-11 16:52:14 -0500

    updated solution for clarity / testabality

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,29 +1,25 @@
-//Get answer and attempt inputs for use throughout page
-let answer = document.getElementById('answer').value;
-let attempt = document.getElementById('attempt').value;
+let answer = document.getElementById('answer');
+let attempt = document.getElementById('attempt');
 
-function guess(){
-    //Get user input
-    let input = document.getElementById('user-guess').value;
-
-    //Set Answer and Attempt if either is empty (new game)
-    if(answer == "" || attempt == "") {
+function guess() {
+    let input = document.getElementById('user-guess');
+    if(answer.value == "" || attempt.value == "") {
         setHiddenFields();
     }
 
     //Ensure user input is valid, iterate attempt if valid
-    if(!validateInput(input)){
+    if(!validateInput(input.value)){
         return;
     } else {
-        attempt++;
+        attempt.value++;
     }
 
     //Check user input against answer and display results
-    if(getResults(input)) {
+    if(getResults(input.value)) {
         setMessage('You Win! :)');
         showAnswer(true);
         showReplay();
-    } else if(attempt >= 10) {
+    } else if(attempt.value >= 10) {
         setMessage('You Lose! :(');
         showAnswer(false);
         showReplay();
@@ -38,11 +34,11 @@ function getResults(input){
     let html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
     for(i = 0; i < input.length; i++)
     {
-        if(input.charAt(i) == answer.charAt(i))
+        if(input.charAt(i) == answer.value.charAt(i))
         {
             html += '<span class="glyphicon glyphicon-ok"></span>';
             correct++;
-        } else if (answer.indexOf(input.charAt(i)) > -1) {
+        } else if (answer.value.indexOf(input.charAt(i)) > -1) {
             html += '<span class="glyphicon glyphicon-transfer"></span>';
         } else {
             html += '<span class="glyphicon glyphicon-remove"></span>';
@@ -61,11 +57,11 @@ function getResults(input){
 
 //function sets hidden fields
 function setHiddenFields() {
-    answer = Math.floor(Math.random() * 10000).toString();
-    while(answer.length < 4) {
-        answer = "0" + answer;
+    answer.value = Math.floor(Math.random() * 10000).toString();
+    while(answer.value.length < 4) {
+        answer.value = "0" + answer.value;
     }
-    attempt = 0;
+    attempt.value = 0;
 }
 
 //function sets the message to supplied string
@@ -81,13 +77,13 @@ function showAnswer(success){
     } else {
         code.className += " failure";
     }
-    code.innerHTML = answer;
+    code.innerHTML = answer.value;
 }
 
 //function hides the guessing-div and shows the replay-div to allow replay
 function showReplay(){
-    document.getElementById('guessing-div').style = "display:none";
-    document.getElementById('replay-div').style = "display:block";
+    document.getElementById('guessing-div').style.display = "none";
+    document.getElementById('replay-div').style.display = "block";
 }
 
 //function ensure input was 4 characters long
