58eb641f60729239ef1c167e64ed4044f65b6562
Eric J Fisher <EricJFisher@Live.com>
2017-01-11 16:45:01 -0500

    corrected various issues throughout tasks

--- test/main.spec.js
+++ test/main.spec.js
@@ -23,7 +23,6 @@ describe('CodeBreaker :', () => {
 
   let document;
   let window;
-  var array = [];
 
   // Setup
   before((done) => {
@@ -43,22 +42,23 @@ describe('CodeBreaker :', () => {
         done();
       }, 2000)
     });
-
-    // don't prepopulate or data unless `setHiddenFields` is a valid function
-    if (typeof window.setHiddenFields === "function") {
-        for(var i = 0; i < 10; i++)
-        {
-          window.setHiddenFields();
-          array.push(document.getElementById('answer').value);
-        }
-        array.sort();
-    }
   });
 
   // Tests
   describe('setHiddenFields()', function() {
     it('should set element `answer` to a random whole number between 0 and 9999',function() {
       assert(typeof window.setHiddenFields === "function",'a fuction named `setHiddenFields` was not found.');
+
+      var array = [];
+      if (typeof window.setHiddenFields === 'function') {
+        for(var i = 0; i < 10; i++)
+        {
+          window.setHiddenFields();
+          array.push(document.getElementById('answer').value);
+        }
+        array.sort();
+      }
+
       assert(array.length > 0, '`answer` was not provided a value.');
       var current = null;
       var duplicates = 0;
@@ -76,6 +76,17 @@ describe('CodeBreaker :', () => {
 
     it('should set element `answer` to a number exactly 4 characters long.', function() {
       assert(typeof window.setHiddenFields === "function",'a fuction named `setHiddenFields` was not found.');
+
+      var array = [];
+      if (typeof window.setHiddenFields === 'function') {
+        for(var i = 0; i < 10; i++)
+        {
+          window.setHiddenFields();
+          array.push(document.getElementById('answer').value.toString());
+        }
+        array.sort();
+      }
+
       assert(array.length > 0, '`answer` was not provided a value.');
       for(var i = 0; i < array.length; i++) {
         assert(array[i].length == 4, '`answer` should have a value exactly 4 characters long.');
@@ -101,24 +112,24 @@ describe('CodeBreaker :', () => {
 
   describe('setMessage()', function(){
     it('should accept one parameter and set the `innerHTML` of element `message` to that parameter.', function(){
-      assert(typeof window.setMessage === "function",'a fuction named `setMessage` was not found.')
+      assert(typeof window.setMessage === "function",'a fuction named `setMessage` was not found.');
       window.setMessage('test `setMessage` function.');
-      assert(document.getElementById('message').value == 'test `setMessage` function.', '`message`s `innerHTML` did not match the provided value.');
+      assert(document.getElementById('message').innerHTML == 'test `setMessage` function.', '`message`s `innerHTML` did not match the provided value.');
     });
   });
 
   describe('validateInput()', function(){
     it('should accept one parameter and return `true` only when that parameter\'s length is 4.', function() {
       assert(typeof window.validateInput === "function",'a fuction named `validateInput` was not found.');
-      assert(window.validateInput(1234) == true,'did not return `true` when valid input was provided.');
-      assert(window.validateInput(123) == false, 'returned `true` when length was not 4.');
+      assert(window.validateInput('1234') == true,'did not return `true` when valid input was provided.');
+      assert(window.validateInput('123') == false, 'returned `true` when length was not 4.');
     });
 
     it('should run when `guess` runs', function(){
       assert(typeof window.validateInput === "function",'a fuction named `validateInput` was not found.');
       var spy = sinon.spy(window, "validateInput");
       window.guess();
-      assert(spy.calledOnce(),'was not run when `guess` ran.');
+      assert(window.validateInput.calledOnce,'was not run when `guess` ran.');
     });
   });
 
@@ -127,51 +138,58 @@ describe('CodeBreaker :', () => {
       assert(typeof window.getResults === "function",'a fuction named `getResults` was not found.');
       //Setup
       document.getElementById('results').innerHTML = '';
-      document.getElementById('answer').value = 1234;
+      document.getElementById('answer').value = '1234';
       //first character correct, second wrong place, third and fouth wrong
-      window.getResults(1300);
+      window.getResults('1300');
       //add tests for starting and stopping divs?
       //add tests for each result?
-      assert(document.getElementById('results').innerHTML.indexOf('glyphicon glyphicon-ok') != -1, 'correct characters are not being marked as correct.');
-      assert(!document.getElementById('results').innerHTML.indexOf('glyphicon glyphicon-ok') > 0, 'some characters are being incorrectly marked as correct.');
-      assert(document.getElementById('results').innerHTML.indexOf('glyphicon glyphicon-transfer') != 1, 'characters that were present in the answer, but not in the right position are not getting marked correctly.');
-      assert(!document.getElementById('results').innerHTML.indexOf('glyphicon glyphicon-transfer') > 0, 'some characters are being incorrectly marked as in the answer, but not in the right position.');
-      assert(document.getElementById('results').innerHTML.indexOf('glyphicon glyphicon-remove') != -1, 'characters not present in the answer are not being marked as wrong.');
-      assert(!document.getElementById('results').innerHTML.indexOf('glyphicon glyphicon-remove') > 1, 'some characters are being incorrectly marked as not in the awnser.');
+
+      var oks = (document.getElementById('results').innerHTML.match(/glyphicon-ok/g) || []).length;
+      var transfers = (document.getElementById('results').innerHTML.match(/glyphicon-transfer/g) || []).length;
+      var removes = (document.getElementById('results').innerHTML.match(/glyphicon-remove/g) || []).length;
+
+      assert(oks > 0, 'correct characters are not being marked as correct.');
+      assert(oks < 2, 'some characters are being incorrectly marked as correct.');
+      assert(transfers > 0, 'characters that were present in the answer, but not in the right position are not getting marked correctly.');
+      assert(transfers < 2, 'some characters are being incorrectly marked as in the answer, but not in the right position.');
+      assert(removes > 1, 'characters not present in the answer are not being marked as wrong.');
+      assert(removes < 3, 'some characters are being incorrectly marked as not in the awnser.');
     });
 
     it('should return `true` if element `answer`\'s `value` and parameter match, otherwise return `false`', function(){
       assert(typeof window.getResults === "function",'a fuction named `getResults` was not found.');
-      document.getElementById('answer').value = 1234;
+      document.getElementById('answer').value = '1234';
       assert(window.getResults('1234'), 'did not return `true` when parameter matched element `answer`\'s `value`.');
       assert(!window.getResults('0000'), 'did not return `false` whene parameter did not match element `answer`\'s `value`.');
     });
+  });
 
-    it('should call `setMessage` with value "You win! :)" when `getResults` returns `true`.', function(){
+  describe('guess()', function(){
+    it('should call `setMessage` with value "You Win! :)" when `getResults` returns `true`.', function(){
       assert(typeof window.getResults === "function",'a fuction named `getResults` was not found.');
-      document.getElementById('answer').value = 1234;
-      document.getElementById('attempt').value = 1;
-      document.getElementById('user-guess').value = 1234;
+      document.getElementById('answer').value = '1234';
+      document.getElementById('attempt').value = '1';
+      document.getElementById('user-guess').value = '1234';
       window.guess();
-      assert(document.getElementById('message').value == 'You win! :)', 'didn\'t set the message to "You win! :)" when user input matched element `answer`\'s `value`');
+      assert(document.getElementById('message').innerHTML == 'You Win! :)', 'didn\'t set the message to "You Win! :)" when user input matched element `answer`\'s `value`');
     });
 
     it('should call `setMessage` with the value "You Lose! :(" when element `attempt`\'s `value` is 10 or more and `getResurts` returns false.', function(){
       assert(typeof window.getResults === "function",'a fuction named `getResults` was not found.');
-      document.getElementById('answer').value = 1234;
-      document.getElementById('attempt').value = 10;
-      document.getElementById('user-guess').value = 4321;
+      document.getElementById('answer').value = '1234';
+      document.getElementById('attempt').value = '10';
+      document.getElementById('user-guess').value = '4321';
       window.guess();
-      assert(document.getElementById('message').value == 'You Lose! :(', 'didn\'t set the message to "You Lose! :(" when user input did not match element `answer`\'s `value` and `attempt`\'s `value` was 10 or more.');
+      assert(document.getElementById('message').innerHTML == 'You Lose! :(', 'didn\'t set the message to "You Lose! :(" when user input did not match element `answer`\'s `value` and `attempt`\'s `value` was 10 or more.');
     });
 
     it('should call `setMessage` with the value "Incorrect, try again." when element `awnser`\'s `value` does not match user input, but `attempt`\'s `value` is less than 10.', function(){
       assert(typeof window.getResults === "function",'a fuction named `getResults` was not found.');
-      document.getElementById('answer').value = 1234;
-      document.getElementById('attempt').value = 1;
-      document.getElementById('user-guess').value = 4321;
+      document.getElementById('answer').value = '1234';
+      document.getElementById('attempt').value = '1';
+      document.getElementById('user-guess').value = '4321';
       window.guess();
-      assert(document.getElementById('message').value == 'Incorrect, try again.', 'didn\'t set the message to "Incorrect, try again." when user input did not match element `answer`\'s `value` and `attempt`\'s `value` was less than 10.');
+      assert(document.getElementById('message').innerHTML == 'Incorrect, try again.', 'didn\'t set the message to "Incorrect, try again." when user input did not match element `answer`\'s `value` and `attempt`\'s `value` was less than 10.');
     });
   });
 
@@ -183,13 +201,13 @@ describe('CodeBreaker :', () => {
       window.setHiddenFields();
       window.showAnswer(true);
       assert(document.getElementById('answer').value == code.innerHTML, '`code`\'s `innerHTML` did not match the `awnser`\'s value when parameter was `true`.');
-      assert(code.className.indexOf(' success') == 0,'`code`\'s `className` did not have ` success` in it when parameter was `true`.');
+      assert(code.className.indexOf(' success') != -1,'`code`\'s `className` did not have ` success` in it when parameter was `true`.');
       //test false
       code.value = '';
       code.className = '';
       window.showAnswer(false);
       assert(document.getElementById('answer').value == code.innerHTML, '`code`\'s `innerHTML` did not match the `awnser`\'s value when parameter was `false`.');
-      assert(code.className.indexOf(' failure') == 0,'`code`\'s `className` did not have ` failure` in it when parameter was `false`.');
+      assert(code.className.indexOf(' failure') != -1,'`code`\'s `className` did not have ` failure` in it when parameter was `false`.');
     });
   });
 
@@ -197,8 +215,8 @@ describe('CodeBreaker :', () => {
     it('should set element `guessing-div`\'s `style` to "display:none" element `replay-div`\'s `style` to "display:block".', function(){
       assert(typeof window.showReplay === "function",'a fuction named `showReplay` was not found.');
       window.showReplay();
-      assert(document.getElementById('guessing-div').style == "display:none", '`guessing-div`\'s `style` was not set to "display:none".');
-      assert(document.getElementById('replay-div').style == "display:block", '`replay-div`\'s `style` was not set to "display:block".');
+      assert(document.getElementById('guessing-div').style.display == "none", '`guessing-div`\'s `style` was not set to "display:none".');
+      assert(document.getElementById('replay-div').style.display == "block", '`replay-div`\'s `style` was not set to "display:block".');
     });
   });
 
@@ -207,19 +225,19 @@ describe('CodeBreaker :', () => {
       assert(typeof window.showAnswer === "function",'a fuction named `showAnswer` was not found.');
       assert(typeof window.showReplay === "function",'a fuction named `showReplay` was not found.');
       //win outcome
-      document.getElementById('answer').value = 1234;
-      document.getElementById('attempt').value = 1;
-      document.getElementById('user-guess').value = 1234;
+      document.getElementById('answer').value = '1234';
+      document.getElementById('attempt').value = '1';
+      document.getElementById('user-guess').value = '1234';
       window.guess();
-      assert(document.getElementById('guessing-div').style == "display:none", '`showReplay` was not run when game win condition was met.');
-      assert(document.getElementById('message').value == 'You win! :)','`showAnswer` was not run when game win condition was met');
+      assert(document.getElementById('guessing-div').style.display == "none", '`showReplay` was not run when game win condition was met.');
+      assert(document.getElementById('message').innerHTML == 'You Win! :)','`showAnswer` was not run when game win condition was met');
       //lose outcame
-      document.getElementById('answer').value = 1234;
-      document.getElementById('attempt').value = 10;
-      document.getElementById('user-guess').value = 4321;
+      document.getElementById('answer').value = '1234';
+      document.getElementById('attempt').value = '10';
+      document.getElementById('user-guess').value = '4321';
       window.guess();
-      assert(document.getElementById('guessing-div').style == "display:none", '`showReplay` was not run when game lose condition was met.');
-      assert(document.getElementById('message').value == 'You Lose! :(','`showAnswer` was not run when game lose condition was met');
+      assert(document.getElementById('guessing-div').style.display == "none", '`showReplay` was not run when game lose condition was met.');
+      assert(document.getElementById('message').innerHTML == 'You Lose! :(','`showAnswer` was not run when game lose condition was met');
     });
   });
 });
