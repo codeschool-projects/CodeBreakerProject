eee8c1d79fa06ec157fd202c808d2868bf978425
María Belén Cattaneo <belcattaneo@gmail.com>
2017-04-06 16:30:06 -0300

    terminado pero no anda

--- src/assets/main.js
+++ src/assets/main.js
@@ -13,6 +13,18 @@ function guess() {
    } else {
        attempt++;
    }
+
+   if ( getResults ){
+       setMessage("You Win! :)");
+       showAnswer(true);
+       showReplay();
+   } else if (attempt >= 10){
+       setMessage("You Lose! :(");
+       showAnswer(false);
+       showReplay();
+   } else {
+       setMessage("Incorrect, try again.");
+   }
 }
 
 //implement new functions here
@@ -41,10 +53,42 @@ function validateInput (input){
     }
 }
 
-function getResults (guess){
+function getResults (input){
     let div = document.getElementById("results");
-        
+    let result = "";
+    let position = "";
+    let correctCharacters = 0;
+
+    for (let i=0 ; i <= input.length ; i++ ){
+        if ( input.charAt(i) == answer.charAt(i) ){
+            position = `<span class="glyphicon glyphicon-ok"></span>`;
+            correctCharacters++;
+        } else if ( answer.includes( input.charAt(i) ) ){
+            position =  `<span class="glyphicon glyphicon-transfer"></span>`;
+        } else{
+            position = `<span class="glyphicon glyphicon-remove"></span>`;
+        }
+        result += position;
+    }
+        div.innerHTML = `<div class="row"><span class="col-md-6">${input}</span><div class="col-md-6"> ${result} </div>`;    
+    
+    return (correctCharacters === input.length);
 }
 
+function showAnswer (param){
+    let label = document.getElementById("code");
+    let classes = label.getAttribute("class");
 
+    if (param){
+        label.innerHTML = document.getElementById("answer").val;
+        label.setAttribute(`${classes} success`);
+    } else {
+        label.setAttribute(`${classes} failure`);;
+    }
+}
+
+function showReplay (){
+    document.getElementById("guessing-div").style.display = "none";
+    document.getElementById("reply-div").style.display = "block";
+}
 
