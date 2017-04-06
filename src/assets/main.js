a91eb9d442522f8f03394bb01f8dd7612ab63c84
mattcritelli <matthew.critelli@gmail.com>
2017-04-06 11:34:42 -0600

    Create  setMessage function to display correct message to user.  Validate user input.  Update main guess function to incorporate latest features.

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,11 +2,40 @@ let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
 
 function guess() {
-    let input = document.getElementById('user-guess');
+  let input = document.getElementById('user-guess');
+
+  if(answer.value === '' || attempt.value === ''){
+    setHiddenFields();
+  }
+
+  if(!validateInput(input.value)){
+    return false;
+  }
+  attempt.value++
 }
+
+//Use JS Math.random function to create a random number
+// Multiply by 10000 and then round down using Math.floor method
+// Finally, convert to string to confirm 4 digit length and modify if necessary
 
 function setHiddenFields(){
-    let answer = (Math.random() * 1000)
+  answer.value = Math.floor(Math.random() * 10000).toString();
+  attempt.value = 0;
+
+  while(answer.value.length < 4){
+    answer.value += "0";
+  }
+  return answer.value
 }
 
-//implement new functions here
+function setMessage(message){
+  document.getElementById('message').innerHTML = message
+}
+
+function validateInput(input){
+  if(input.length !== 4){
+    setMessage('Guesses must be exactly 4 characters long.');
+    return false;
+  }
+  return true;
+}
