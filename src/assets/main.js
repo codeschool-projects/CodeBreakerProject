319827f2129523944aa37359cdb6cf42d25766e0
mattcritelli <matthew.critelli@gmail.com>
2017-04-06 11:17:39 -0600

    Add function to set random 4 digit number and convert to string.  Add guess function which will set random number if none exists yet.  Set counter to track attempts at 0.

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,10 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+}
+
+function setHiddenFields(){
+    let answer = (Math.random() * 1000)
 }
 
 //implement new functions here
