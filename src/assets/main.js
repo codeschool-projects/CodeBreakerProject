ac596aa40628bbc69bf9f70d91c695453d7d02a2
Basts <38281922+Basts@users.noreply.github.com>
2018-04-18 11:19:40 +0200

    dd

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,3 +7,9 @@ function guess() {
 }
 
 //implement new functions here
+function setHiddenFields(){
+  answer.value = Mat.floor(Math.random()*10000).toString();
+  while(answer.value.length<4){
+     answer.value = "0" + answer.value;
+  }
+}
