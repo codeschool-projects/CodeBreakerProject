be42b1d7a9ebcf57d8957b3add2f1bec8957b06c
Basts <38281922+Basts@users.noreply.github.com>
2018-04-18 11:22:33 +0200

    poprawka

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,7 +8,7 @@ function guess() {
 
 //implement new functions here
 function setHiddenFields(){
-  answer.value = Mat.floor(Math.random()*10000).toString();
+  answer.value = Math.floor(Math.random()*10000).toString();
   while(answer.value.length<4){
      answer.value = "0" + answer.value;
   }
